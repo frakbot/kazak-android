f319a72690d6dd411eda951526c1483a57f59f23
Sebastiano Poggi <poggos@gmail.com>
2015-07-08 00:20:16 +0100

    Add basic UI chrome to schedule activity

--- DroidconApp/app/src/main/java/com/droidcon/uk/app/ScheduleActivity.java
+++ DroidconApp/app/src/main/java/com/droidcon/uk/app/ScheduleActivity.java
@@ -1,7 +1,13 @@
 package com.droidcon.uk.app;
 
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.droidcon.uk.app.model.Schedule;
 
@@ -13,20 +19,50 @@ import rx.subscriptions.CompositeSubscription;
 
 public class ScheduleActivity extends AppCompatActivity {
 
-    private final CompositeSubscription subscriptions = new CompositeSubscription();
+    private final CompositeSubscription subscriptions;
 
     @Inject
     DataRepository dataRepository;
+
+    private FrameLayout contentRootView;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+
+    public ScheduleActivity() {
+        subscriptions = new CompositeSubscription();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         DroidconApplication.injector().inject(this);
+        setContentView(R.layout.activity_schedule);
+        contentRootView = (FrameLayout) findViewById(R.id.content_root);
+        drawerLayout = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        navigationView = (NavigationView) findViewById(R.id.drawer_menu);
+        toolbar = (Toolbar) findViewById(R.id.appbar);
+
+        setupAppBar();
+    }
+
+    private void setupAppBar() {
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(navigationView);
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        subscribeToSchedule();
+    }
+
+    private void subscribeToSchedule() {
         subscriptions.add(
                 dataRepository.getSchedule()
                         .observeOn(AndroidSchedulers.mainThread())
@@ -41,19 +77,26 @@ public class ScheduleActivity extends AppCompatActivity {
     }
 
     private void updateWith(Schedule schedule) {
-
+        // TODO
     }
 
     private class ScheduleObserver implements Observer<Schedule> {
 
         @Override
         public void onCompleted() {
-
+            // No-op
         }
 
         @Override
         public void onError(Throwable e) {
-
+            Snackbar.make(contentRootView, R.string.error_loading_schedule, Snackbar.LENGTH_LONG)
+                    .setAction(R.string.action_retry, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            subscribeToSchedule();
+                        }
+                    })
+                    .show();
         }
 
         @Override
@@ -61,4 +104,5 @@ public class ScheduleActivity extends AppCompatActivity {
             updateWith(schedule);
         }
     }
+
 }
