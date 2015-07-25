ca53258d37b476f33068e9eb2927851e012351dd
Daniele Bonaldo <dany.bony@gmail.com>
2015-07-25 11:33:06 +0100

    Add notifier

---
+++ DroidconApp/app/src/main/java/uk/co/droidcon/kazak/notifications/Notifier.java
@@ -0,0 +1,25 @@
+package uk.co.droidcon.kazak.notifications;
+
+import android.app.Notification;
+import android.content.Context;
+import android.support.v4.app.NotificationManagerCompat;
+
+public class Notifier {
+
+    private static final int SINGLE_NOTIFICATION_ID = 42;
+
+    private final NotificationManagerCompat notificationManagerCompat;
+
+    public static Notifier from(Context context) {
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        return new Notifier(notificationManagerCompat);
+    }
+
+    private Notifier(NotificationManagerCompat notificationManagerCompat) {
+        this.notificationManagerCompat = notificationManagerCompat;
+    }
+
+    public void showNotification(Notification singleNotification) {
+        notificationManagerCompat.notify(SINGLE_NOTIFICATION_ID, singleNotification);
+    }
+}
