171bdf112cd5be3c316750e892ae62a30dcd6094
Sebastiano Poggi <poggos@gmail.com>
2015-08-07 00:28:14 +0100

    Create TestUtils, refactor tests

--- DroidconApp/core/src/test/java/uk/co/droidcon/kazak/repository/DroidconDataRepositoryTest.kt
+++ DroidconApp/core/src/test/java/uk/co/droidcon/kazak/repository/DroidconDataRepositoryTest.kt
@@ -1,23 +1,24 @@
 package uk.co.droidcon.kazak.repository
 
-import org.fest.assertions.api.Assertions
-import org.junit.Before as before
-import org.junit.Test as test
 import org.mockito.Mockito
 import rx.subjects.BehaviorSubject
 import uk.co.droidcon.kazak.api.DroidconApi
+import uk.co.droidcon.kazak.assertThat
 import uk.co.droidcon.kazak.model.*
-import java.util.Date
+import uk.co.droidcon.kazak.verify
+import java.util.*
+import org.junit.Before as before
+import org.junit.Test as test
 
 public class DroidconDataRepositoryTest {
 
     val talkId = "TestId"
     val testDate = Date()
 
-    val mockApi : DroidconApi = Mockito.mock(javaClass())
-    val scheduleObservable : BehaviorSubject<Schedule> = BehaviorSubject.create()
+    val mockApi: DroidconApi = Mockito.mock(javaClass())
+    val scheduleObservable: BehaviorSubject<Schedule> = BehaviorSubject.create()
 
-    val repository : DataRepository = DroidconDataRepository(mockApi)
+    val repository: DataRepository = DroidconDataRepository(mockApi)
 
     before
     fun setup() {
@@ -27,24 +28,31 @@ public class DroidconDataRepositoryTest {
 
     test
     fun itFetchesAScheduleFromApiIfMemoryCacheEmpty() {
-        val schedule = repository.getSchedule().toBlocking().first();
+        val schedule = repository.getSchedule().toBlocking().first()
 
-        Mockito.verify(mockApi).fetchSchedule()
-        Assertions.assertThat(schedule).isEqualTo(testSchedule())
+        verify(mockApi).fetchSchedule()
+        assertThat(schedule).isEqualTo(testSchedule())
     }
 
     test
     fun itFindsATalkGivenAnId() {
-        val talk = repository.getTalk(talkId).toBlocking().first();
+        val talk = repository.getTalk(talkId).toBlocking().first()
 
-        Assertions.assertThat(talk).isEqualTo(testTalk())
+        assertThat(talk).isEqualTo(testTalk())
     }
 
     private fun testSchedule() = Schedule(listOf(testDay()))
 
     private fun testDay() = Day(testDate, listOf(testTalk()))
 
-    private fun testTalk() = Talk(talkId, "", TimeSlot(testDate, testDate), Room("", ""))
+    private fun testTalk() = Talk(talkId, "", TimeSlot(testDate, testDate), Room("", ""), testSpeakers())
 
+    private fun testSpeakers() = Speakers(testSpeakersList())
+
+    private fun testSpeakersList(): List<Speaker> {
+        val speakers = ArrayList<Speaker>(1)
+        speakers[0] = Speaker("", "")
+        return speakers
+    }
 
 }
