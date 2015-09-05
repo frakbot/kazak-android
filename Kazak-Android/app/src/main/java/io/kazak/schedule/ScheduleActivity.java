d0453444461461e4bab583b1c8653256271e12b3
Daniele Bonaldo <dany.bony@gmail.com>
2015-09-05 14:00:59 +0100

    Suppress checkstyle:visibilitymodifier

--- Kazak-Android/app/src/main/java/io/kazak/schedule/ScheduleActivity.java
+++ Kazak-Android/app/src/main/java/io/kazak/schedule/ScheduleActivity.java
@@ -28,7 +28,7 @@ public class ScheduleActivity extends AppCompatActivity {
     private final CompositeSubscription subscriptions;
 
     @Inject
-    @SuppressWarnings("checkstyle:visibilitymodifiercheck")
+    @SuppressWarnings("checkstyle:visibilitymodifier")
     DataRepository dataRepository;
 
     private FrameLayout contentRootView;
