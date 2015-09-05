ba31142af438db109708236f4e2b36e3bad76756
Daniele Bonaldo <dany.bony@gmail.com>
2015-09-05 12:58:18 +0100

    Use stacked notifications and summary for wear

--- Kazak-Android/app/src/main/java/io/kazak/notifications/NotificationCreator.java
+++ Kazak-Android/app/src/main/java/io/kazak/notifications/NotificationCreator.java
@@ -17,6 +17,9 @@ import io.kazak.schedule.ScheduleActivity;
 
 public class NotificationCreator {
 
+    private static final String GROUP_KEY_NOTIFY_SESSION = "group_key_notify_session";
+    private static final int STACKED_NOTIFICATIONS_ID_OFFSET = 1;
+
     // pulsate every 1 second, indicating a relatively high degree of urgency
     private static final int NOTIFICATION_LED_ON_MS = 100;
     private static final int NOTIFICATION_LED_OFF_MS = 1000;
@@ -32,22 +35,41 @@ public class NotificationCreator {
         for (Talk talk : talks) {
             notifications.add(createFrom(talk));
         }
+        if (talks.size() > 1) {
+            notifications.add(createSummaryNotification(talks));
+        }
         return notifications;
     }
 
     private Notification createFrom(Talk talk) {
-        NotificationCompat.Builder summaryBuilder = createDefaultBuilder();
-        summaryBuilder
+        NotificationCompat.Builder notificationBuilder = createDefaultBuilder(1);
+        notificationBuilder
                 .setContentIntent(createPendingIntentForSingleSession(talk.getId()))
                 .setContentTitle(talk.getName())
-                .setContentText(talk.getRoom().getName());
+                .setContentText(talk.getRoom().getName())
+                .setGroup(GROUP_KEY_NOTIFY_SESSION);
 
-        NotificationCompat.BigTextStyle richNotification = createBigTextRichNotification(summaryBuilder, talk);
+        NotificationCompat.BigTextStyle richNotification = createBigTextRichNotification(notificationBuilder, talk);
 
         return richNotification.build();
     }
 
-    private NotificationCompat.Builder createDefaultBuilder() {
+    private Notification createSummaryNotification(List<Talk> talks) {
+        NotificationCompat.Builder summaryBuilder = createDefaultBuilder(talks.size());
+        summaryBuilder
+                .setContentIntent(createPendingIntentForMultipleSessions())
+                .setContentTitle("AAAAAA")
+                .setContentText("BBBBBB")
+                .setGroup(GROUP_KEY_NOTIFY_SESSION)
+                .setGroupSummary(true)
+                .setLocalOnly(true);
+
+        NotificationCompat.InboxStyle richNotification = createInboxStyleRichNotification(summaryBuilder, talks);
+
+        return richNotification.build();
+    }
+
+    private NotificationCompat.Builder createDefaultBuilder(int talksCount) {
         Resources resources = context.getResources();
 
         NotificationCompat.WearableExtender extender = new NotificationCompat.WearableExtender();
@@ -57,6 +79,13 @@ public class NotificationCreator {
         return new NotificationCompat.Builder(context)
                 // TODO: use topic color
                 //.setColor(resources.getColor(R.color.theme_primary))
+                .setTicker(
+                        context.getResources().getQuantityString(
+                                R.plurals.session_notification_ticker,
+                                talksCount,
+                                talksCount
+                        )
+                )
                 .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE)
                 .setLights(
                         resources.getColor(R.color.notification_led_color),
@@ -77,6 +106,11 @@ public class NotificationCreator {
         return taskBuilder.getPendingIntent(0, PendingIntent.FLAG_CANCEL_CURRENT);
     }
 
+    private PendingIntent createPendingIntentForMultipleSessions() {
+        TaskStackBuilder taskBuilder = createBaseTaskStackBuilder();
+        return taskBuilder.getPendingIntent(0, PendingIntent.FLAG_CANCEL_CURRENT);
+    }
+
     private TaskStackBuilder createBaseTaskStackBuilder() {
         Intent baseIntent = new Intent(context, ScheduleActivity.class);
         baseIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
@@ -85,15 +119,33 @@ public class NotificationCreator {
     }
 
     private NotificationCompat.BigTextStyle createBigTextRichNotification(
-            NotificationCompat.Builder notifBuilder, Talk talk) {
-        String speakers = talk.getSpeakersNames(); // TODO: get the actual ones from the session
+            NotificationCompat.Builder notificationBuilder, Talk talk) {
+        String speakers = talk.getSpeakersNames();
         String roomName = talk.getRoom().getName();
         StringBuilder bigTextBuilder = new StringBuilder()
                 .append(context.getString(R.string.session_notification_starting_by, speakers))
                 .append('\n')
                 .append(context.getString(R.string.session_notification_starting_in, roomName));
-        return new NotificationCompat.BigTextStyle(notifBuilder)
+        return new NotificationCompat.BigTextStyle(notificationBuilder)
                 .setBigContentTitle(talk.getName())
                 .bigText(bigTextBuilder.toString());
+    }
+
+    private NotificationCompat.InboxStyle createInboxStyleRichNotification(
+            NotificationCompat.Builder notificationBuilder, List<Talk> talks) {
+        String bigContentTitle = context.getString(R.string.session_notification_count_starting, talks.size());
+        NotificationCompat.InboxStyle richNotification = new NotificationCompat.InboxStyle(notificationBuilder)
+                .setBigContentTitle(bigContentTitle);
+        for (Talk talk : talks) {
+            richNotification.addLine(
+                    context.getString(
+                            R.string.room_session_notification,
+                            talk.getRoom().getName(),
+                            talk.getName()
+                    )
+            );
+        }
+
+        return richNotification;
     }
 }
