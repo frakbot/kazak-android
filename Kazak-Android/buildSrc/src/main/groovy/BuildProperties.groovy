033c5f881a8f85489d1b95b0684435798a14ff1a
Francesco Pontillo <francescopontillo@gmail.com>
2015-09-06 21:03:51 +0100

    Add AUTH_URL to BuildConfig

--- Kazak-Android/buildSrc/src/main/groovy/BuildProperties.groovy
+++ Kazak-Android/buildSrc/src/main/groovy/BuildProperties.groovy
@@ -95,6 +95,7 @@ class BuildProperties {
         flavor.buildConfigField "double", "VENUE_LOCATION_LON", "${getDouble("venue.longitude")}"
         flavor.buildConfigField "int", "VENUE_LOCATION_MAP_ZOOM", "${getInt("venue.map.defaultZoom")}"
         flavor.buildConfigField "String", "ENDPOINT_URL", "${getString("app.endpoint")}"
+        flavor.buildConfigField "String", "AUTH_URL", "${getString("app.auth")}"
 
         flavor.buildConfigField "boolean", "ENABLE_STETHO", "BuildConfig.DEBUG"
 
