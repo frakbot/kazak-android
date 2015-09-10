90d7a8076513bdef372761068d507c291796e83a
Francesco Pontillo <francescopontillo@gmail.com>
2015-09-10 15:31:38 +0200

    Apply map, filter and distinct via Kotlin Iterable extensions
    Because Kotlin.

--- Kazak-Android/app/src/main/kotlin/io/kazak/login/EmailHelper.kt
+++ Kazak-Android/app/src/main/kotlin/io/kazak/login/EmailHelper.kt
@@ -2,23 +2,19 @@ package io.kazak.login
 
 import android.accounts.AccountManager
 import android.content.Context
-import rx.Observable
 
 public fun getDeviceUserEmails(context: Context): List<String> {
     // fetch and filter emails from the account manager
     val am = AccountManager.get(context)
-    val accounts = am.getAccounts()
-    return Observable.from(accounts)
-            .map<String> {
+    val accounts = am.getAccounts().toList();
+    return accounts
+            .map {
                 it.name
             }
             .filter {
                 isEmailValid(it)
             }
             .distinct()
-            .toList()
-            .toBlocking()
-            .single()
 }
 
 public fun isEmailValid(email: String): Boolean {
