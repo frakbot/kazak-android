890683693c6312e15bc3459cff4325fa10be754e
Eugenio Marletti <takhion@gmail.com>
2015-09-13 20:03:46 +0200

    Add multiple rooms support in ScheduleTableAdapter

--- Kazak-Android/app/src/main/java/io/kazak/schedule/view/table/ScheduleTableViewHolder.java
+++ Kazak-Android/app/src/main/java/io/kazak/schedule/view/table/ScheduleTableViewHolder.java
@@ -1,6 +1,7 @@
 package io.kazak.schedule.view.table;
 
 import android.support.annotation.NonNull;
+import android.support.v4.util.Pair;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.Date;
@@ -10,7 +11,7 @@ import io.kazak.model.Talk;
 import io.kazak.schedule.view.table.base.TableViewHolder;
 import io.kazak.schedule.view.TalkView;
 
-public class ScheduleTableViewHolder extends TableViewHolder<Talk, Room, Date> {
+public class ScheduleTableViewHolder extends TableViewHolder<Pair<Talk, Room>, Room, Date> {
 
     private TalkView talkView;
 
@@ -20,9 +21,9 @@ public class ScheduleTableViewHolder extends TableViewHolder<Talk, Room, Date> {
     }
 
     @Override
-    public void updateWith(Talk item, Room row, Date start, Date end, boolean isPlaceholder) {
+    public void updateWith(Pair<Talk, Room> item, Room row, Date start, Date end, boolean isPlaceholder) {
         super.updateWith(item, row, start, end, isPlaceholder);
-        talkView.updateWith(item);
+        talkView.updateWith(item.first);
     }
 
     TalkView getTalkView() {
