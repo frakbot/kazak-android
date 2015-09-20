d05fed2ccffa78a5538a2759eb9487e9a4d090db
Sebastiano Poggi <poggos@gmail.com>
2015-09-20 23:00:17 +0100

    Postpone navigation to after nav drawer closes

--- Kazak-Android/app/src/main/java/io/kazak/KazakNavDrawerActivity.java
+++ Kazak-Android/app/src/main/java/io/kazak/KazakNavDrawerActivity.java
@@ -1,6 +1,7 @@
 package io.kazak;
 
 import android.annotation.SuppressLint;
+import android.os.Bundle;
 import android.support.annotation.CallSuper;
 import android.support.annotation.IdRes;
 import android.support.annotation.LayoutRes;
@@ -14,6 +15,13 @@ import android.view.Window;
 import io.kazak.base.DeveloperError;
 
 public abstract class KazakNavDrawerActivity extends KazakActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final String EXTRA_FROM_NAV_DRAWER = BuildConfig.APPLICATION_ID + ".extra.from_nav_drawer";
+    private static final Bundle EXTRAS_FROM_NAV_DRAWER = new Bundle(1);
+
+    static {
+        EXTRAS_FROM_NAV_DRAWER.putBoolean(EXTRA_FROM_NAV_DRAWER, true);
+    }
 
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
@@ -87,34 +95,34 @@ public abstract class KazakNavDrawerActivity extends KazakActivity implements Na
 
     @Override
     @CallSuper
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(final MenuItem item) {
         if (item.getItemId() == getNavigationDrawerMenuIdForThisActivity()) {
             // Do nothing: we're already there
             return true;
         }
 
-        switch (item.getItemId()) {
-            case R.id.menu_nav_schedule:
-                navigate().toSchedule();
-                break;
-            case R.id.menu_nav_arrival_info:
-                navigate().toArrivalInfo();
-                break;
-            case R.id.menu_nav_venue_map:
-                navigate().toVenueMap();
-                break;
-            case R.id.menu_nav_settings:
-                navigate().toSettings();
-                break;
-            case R.id.menu_nav_debug:
-                navigate().toDebug();
-                break;
-            default:
-                throw new DeveloperError("Menu item " + item + " not supported");
-        }
-
+        drawerLayout.setDrawerListener(new NavigateAfterDrawerCloseListener(item));
         drawerLayout.closeDrawer(navigationView);
         return true;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        drawerLayout.setDrawerListener(null);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (onBackPressedShouldCloseDrawer()) {
+            drawerLayout.closeDrawer(navigationView);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private boolean onBackPressedShouldCloseDrawer() {
+        return drawerLayout.isDrawerOpen(navigationView);
     }
 
     @IdRes
@@ -124,4 +132,49 @@ public abstract class KazakNavDrawerActivity extends KazakActivity implements Na
         drawerLayout.openDrawer(navigationView);
     }
 
+    private class NavigateAfterDrawerCloseListener implements DrawerLayout.DrawerListener {
+        private final MenuItem item;
+
+        public NavigateAfterDrawerCloseListener(MenuItem item) {
+            this.item = item;
+        }
+
+        @Override
+        public void onDrawerSlide(View drawerView, float slideOffset) {
+            // No-op
+        }
+
+        @Override
+        public void onDrawerOpened(View drawerView) {
+            // No-op
+        }
+
+        @Override
+        public void onDrawerClosed(View drawerView) {
+            switch (item.getItemId()) {
+                case R.id.menu_nav_schedule:
+                    navigate().toSchedule();
+                    break;
+                case R.id.menu_nav_arrival_info:
+                    navigate().toArrivalInfo();
+                    break;
+                case R.id.menu_nav_venue_map:
+                    navigate().toVenueMap();
+                    break;
+                case R.id.menu_nav_settings:
+                    navigate().toSettings();
+                    break;
+                case R.id.menu_nav_debug:
+                    navigate().toDebug();
+                    break;
+                default:
+                    throw new DeveloperError("Menu item " + item + " not supported");
+            }
+        }
+
+        @Override
+        public void onDrawerStateChanged(int newState) {
+            // No-op
+        }
+    }
 }
