ff67bab84d44a70980c856f3848c3b009a7c765b
Sebastiano Poggi <poggos@gmail.com>
2015-09-20 19:29:13 +0100

    Create Navigator

---
+++ Kazak-Android/app/src/main/java/io/kazak/navigation/Navigator.java
@@ -0,0 +1,57 @@
+package io.kazak.navigation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+
+import io.kazak.DebugActivity;
+import io.kazak.map.VenueMapActivity;
+import io.kazak.model.Id;
+import io.kazak.schedule.ScheduleActivity;
+import io.kazak.settings.SettingsActivity;
+import io.kazak.talk.TalkDetailsActivity;
+
+public class Navigator {
+
+    private final Activity activityContext;
+
+    public Navigator(@NonNull Activity activityContext) {
+        this.activityContext = activityContext;
+    }
+
+    public void toSchedule() {
+        Intent intent = new Intent(activityContext, ScheduleActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        activityContext.startActivity(intent);
+    }
+
+    public void toSessionDetails(Id sessionId) {
+        Intent intent = new Intent(activityContext, ScheduleActivity.class);
+        intent.putExtra(TalkDetailsActivity.EXTRA_TALK_ID, sessionId.getId());
+        activityContext.startActivity(intent);
+    }
+
+    public void toArrivalInfo() {
+        // TODO go to arrival info activity once it's created
+        /*Intent intent = new Intent(activityContext, ArrivalInfoActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        activityContext.startActivity(intent);*/
+    }
+
+    public void toVenueMap() {
+        Intent intent = new Intent(activityContext, VenueMapActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        activityContext.startActivity(intent);
+    }
+
+    public void toSettings() {
+        Intent intent = new Intent(activityContext, SettingsActivity.class);
+        activityContext.startActivity(intent);
+    }
+
+    public void toDebug() {
+        Intent intent = new Intent(activityContext, DebugActivity.class);
+        activityContext.startActivity(intent);
+    }
+
+}
